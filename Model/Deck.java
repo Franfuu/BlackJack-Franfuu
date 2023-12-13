82790aa85d3bb19de52eb8c12081a704b8240c6d
Fran <franperezruiz10@gmail.com>
2023-12-13 19:55:18 +0100

    Ultimo commit de verdad

--- Model/Deck.java
+++ Model/Deck.java
@@ -10,7 +10,6 @@ public class Deck {
 
     // Constructor que inicializa el mazo con todas las cartas y las baraja.
     public Deck() {
-        cards = new ArrayList<Card>();
         String[] suits = {"♥", "♦", "♠", "♣"};
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 
